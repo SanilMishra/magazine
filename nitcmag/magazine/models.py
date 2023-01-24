dca2f9f2589f98cd688b375aa9290865403ab593
Gigil James <70816318+gigiljames@users.noreply.github.com>
2023-01-24 12:43:18 +0530

    moments before disaster

--- nitcmag/magazine/models.py
+++ nitcmag/magazine/models.py
@@ -8,4 +8,8 @@ class Login_cred(models.Model):
     Password = models.CharField(max_length=30)
     R_Id = models.IntegerField()
 
+class Reviewer(models.Model):
+    Reviewer_Id = models.CharField(primary_key=True, max_length=9, validators=[MinLengthValidator(9)])
+    Name = models.CharField(max_length=40)
+
 
