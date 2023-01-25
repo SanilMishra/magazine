88b7f648a50a3a6356d2a157118f6a79ac7bbd8e
Gigil James <70816318+gigiljames@users.noreply.github.com>
2023-01-25 18:13:15 +0530

    reviewer table added

--- nitcmag/magazine/models.py
+++ nitcmag/magazine/models.py
@@ -11,5 +11,6 @@ class Login_cred(models.Model):
 class Reviewer(models.Model):
     Reviewer_Id = models.CharField(primary_key=True, max_length=9, validators=[MinLengthValidator(9)])
     Name = models.CharField(max_length=40)
+    
 
 
